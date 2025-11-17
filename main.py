9d5376bf09cc425c9a7ca39fa59d978abb9c7260
Abayie <jonathanboahene@gmail.com>
2025-11-17 20:30:43 +0000

    recap dict

---
+++ main.py
@@ -0,0 +1,11 @@
+#recaping dictionaries3
+programming_dictionary = {
+    "friend": "hate thee",
+    "enemy": "love thee"
+}
+
+print(programming_dictionary["friend"])
+
+
+programming_dictionary["unknown"] = "love or hate thee"
+print(programming_dictionary)
